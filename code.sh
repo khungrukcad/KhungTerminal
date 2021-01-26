c1eb172a3ff8d6b2dbef4912a05254906080a0bf
khungrukcad <75356868+khungrukcad@users.noreply.github.com>
2021-01-26 15:01:04 +0700

    Update code
    Add mention option descript

--- code.sh
+++ code.sh
@@ -1,20 +1,20 @@
-# Written by HakaseShounen
+# Written by khungrukcad
 # August 01, 2016
-# Feel free to contact me at: hakaseshounen@gmail.com
+# Feel free to contact me at: khungrukcad@gmail.com
 # 0.1v
 
-hakaseshounen_hijra () {
+khungrukcad_khung () {
 hs_tarikh='date +%Y%m%d'
 hs_url="http://hijrah.mfrapps.com/api/hijrah-api.php?tarikh=$tarikh"
 hs_xml=$(curl -k --silent "$hs_url")
-hs_hijra_day=$(printf '%s\n' "$hs_xml" | xmlstarlet sel -t -v "date/hijrah/day")
-hs_hijra_month=$(printf '%s\n' "$hs_xml" | xmlstarlet sel -t -v "date/hijrah/month")
-hs_hijra_year=$(printf '%s\n' "$hs_xml" | xmlstarlet sel -t -v "date/hijrah/year")
+hs_khung_day=$(printf '%s\n' "$hs_xml" | xmlstarlet sel -t -v "date/khung/day")
+hs_khung_month=$(printf '%s\n' "$hs_xml" | xmlstarlet sel -t -v "date/khung/month")
+hs_khung_year=$(printf '%s\n' "$hs_xml" | xmlstarlet sel -t -v "date/khung/year")
 hs_masihi_day=$(printf '%s\n' "$hs_xml" | xmlstarlet sel -t -v "date/masihi/day")
 hs_masihi_month=$(printf '%s\n' "$hs_xml" | xmlstarlet sel -t -v "date/masihi/month")
 hs_masihi_month_name=$(printf '%s\n' "$hs_xml" | xmlstarlet sel -t -v "date/masihi/month_name")
 hs_masihi_year=$(printf '%s\n' "$hs_xml" | xmlstarlet sel -t -v "date/masihi/year")
-echo "Today in hijra: $hs_hijra_day $hs_hijra_month $hs_hijra_year"
+echo "Today in khung: $hs_khung_day $hs_khung_month $hs_khung_year"
 echo "Today in masihi: $hs_masihi_day $hs_masihi_month_name $hs_masihi_year"
 }
-hakaseshounen_hijra
+khungrukcad_khung
